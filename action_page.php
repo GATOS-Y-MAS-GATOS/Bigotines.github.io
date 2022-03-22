0cc7e3d88190cf34ebc5a0daef6a955adcc3d442
LANDIN RIVERA MONTSERRAT <99773679+LANDINMONTSERRAT32@users.noreply.github.com>
2022-03-22 17:03:37 -0600

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="BIENVENIDA A GATS Y MÁS GATOS" method="post">
+NOMBRE: <input type="text" NOMBRE="NOMBRE"><br>
+EMAIL: <input type="text" name="EMAIL"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
